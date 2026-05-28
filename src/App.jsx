728b14d7879c0f03d6d77967ac00aa42092b2c5f
Jeonghyeon <mbc5200@gmail.com>
2026-05-28 18:43:43 +0900

    [Feat] React-Router-Dom 연결 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import { Home } from './pages/Home'
+import { Condition } from './pages/Condition'
+import { ConditionMain } from './pages/ConditionMain'
+import { Simulation } from './pages/Simulation'
+import { SimulationMain } from './pages/SimulationMain'
 
 function App() {
 
@@ -10,8 +14,10 @@ function App() {
         <Route path='/' element={<Home/>}/>
 
         {/* element 수정 필요 */}
-        <Route path='/Simulation' element={<Home/>}/>
-        <Route path='/Condition' element={<Home/>}/>
+        <Route path='/Simulation' element={<Simulation />} />
+        <Route path='/SimulationMain' element={<SimulationMain />} />
+        <Route path='/Condition' element={<Condition/>}/>
+        <Route path='/ConditionMain' element={<ConditionMain />} />
         <Route path='/MyPage' element={<Home/>}/>
         <Route path='/Guide' element={<Home/>}/>
       </Routes>
